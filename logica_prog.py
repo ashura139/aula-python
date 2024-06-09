fefa4e37267d287787edeaa43c403ed5f1f1222c
Ashura <themobbossdaniel@gmail.com>
2024-06-08 22:00:55 -0300

    adicionando_arquivos

---
+++ logica_prog.py
@@ -0,0 +1,10 @@
+# if, elif, else, estruras condicionais
+
+x = int(input("digite um valor: "))
+
+if x > 0: 
+    print("x e positivo")
+elif x < 0:
+    print(f"{x}e negativo") 
+else:
+    print("Erro")  
---
+++ tipos_variavel.py
@@ -0,0 +1,4 @@
+nome = "Daniel"
+cidade = "Detroit"
+pais = "Não Só Um Robo"
+print(nome,cidade,pais)
